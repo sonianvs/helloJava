25410374be696258f3302fef48e489abe14226c3
SoniaN <sonia.navas@intenthq.com>
2022-04-24 20:00:42 +0200

    french version added

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hola mon!");
         System.out.println("Hello world!");
         System.out.println("¡Hola mundo!");
+        System.out.println("Salut monde!");
     }
 }
